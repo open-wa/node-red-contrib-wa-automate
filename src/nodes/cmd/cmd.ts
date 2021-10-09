462a44f1ba4678602370f86138125face0151c14
M SHAH <shah@idk.uno>
2021-10-09 04:49:07 +0100

    🐛 attempt to parse possible JSON args

--- src/nodes/cmd/cmd.ts
+++ src/nodes/cmd/cmd.ts
@@ -3,6 +3,24 @@ import { NodeInitializer } from "node-red";
 import { OwaServerNode } from "../owa-server/modules/types";
 import { CmdNode, CmdNodeDef } from "./modules/types";
 
+function tryParseJSONObject (jsonString : unknown){
+  try {
+    //@ts-ignore
+      var o = JSON.parse(jsonString);
+
+      // Handle non-exception-throwing cases:
+      // Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
+      // but... JSON.parse(null) returns null, and typeof null === "object", 
+      // so we must check for that, too. Thankfully, null is falsey, so this suffices:
+      if (o && typeof o === "object") {
+          return o;
+      }
+  }
+  catch (e) { }
+
+  return false;
+};
+
 const nodeInit: NodeInitializer = (RED): void => {
   function CmdNodeConstructor(
     this: CmdNode,
@@ -29,9 +47,12 @@ const nodeInit: NodeInitializer = (RED): void => {
           [k: string]: boolean | string | number
         }
       }
+      const method = m.method || this.method;
+      let argmnts = m.args || this.args;
+      argmnts = tryParseJSONObject(argmnts) || argmnts;
       if (config.server)
         this.server = RED.nodes.getNode(config.server) as OwaServerNode;
-      const executeCommand = () => this.server?.client.ask((m.method || this.method) as keyof Client, m.args || this.args).then(payload => send({
+      const executeCommand = () => this.server?.client.ask(method as keyof Client, argmnts).then(payload => send({
         payload
       })).then(()=>this.status({ fill: 'green', shape: 'dot', text: 'Done' }))
       if (this.server && this.server.client) {
